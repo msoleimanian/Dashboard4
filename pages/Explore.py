07d78e923c56bfd1c2ec8167226089ddb9e5a71a
msoleimanian <36849205+msoleimanian@users.noreply.github.com>
2023-12-11 18:30:17 +0800

    update1

--- pages/Explore.py
+++ pages/Explore.py
@@ -143,6 +143,121 @@ animated_line_html = """
 
 
 st.set_page_config(page_title="Explore", page_icon="📈")
+
+
+
+
+# Function to scale the numbers in a column to a 0-10 range
+def scale_numbers(column_values):
+    max_value = max(column_values)
+    return [round((value / max_value) * 10, 2) for value in column_values]
+
+# Function to get color based on percentage difference from the best
+def get_color(percent_difference):
+    if percent_difference == 0:
+        return 'green'  # Light Red
+    elif 0 < percent_difference <= 1.5:
+        return '#b3ffb3'
+    elif 1.5 < percent_difference <= 3:
+        return 'orange'
+    elif 3 < percent_difference <= 3.5:
+        return '#ff6666'
+
+
+# Data
+seasons = ['Season 1', 'Season 2', 'Season 3']
+plot_numbers = {'Season 1': ['Plot1', 'Plot3', 'Plot4', 'Plot5'],
+                'Season 2': ['Plot1', 'Plot3', 'Plot4', 'Plot5'],
+                'Season 3': ['Plot1', 'Plot3', 'Plot4', 'Plot5']}
+
+columns = ['Plant Height', 'No. of Tiller', 'No. of Panicle', 'No. of Spikelet',
+           'No. of Filled Grain', 'No. Of Unfilled Grain', 'Weight Grain (1000 grains)']
+
+column_score = ['Weight Grain (1000 grains)']
+
+# Create an empty list to store data
+data_rows = []
+
+# Populate the data list for data entry
+for season in seasons:
+    for plot in plot_numbers[season]:
+        row = {'Season': season, 'Plot Number': plot}
+        for col in column_score:
+            row[col] = None  # Initialize with None for data entry
+        data_rows.append(row)
+print(data_rows)
+# Create DataFrame for data entry
+data_entry = pd.DataFrame(data_rows)
+
+# Provide a 2D array to fill in the numbers
+numbers_to_fill = [
+    [98.51, 6, 5,  42, 195, 154, 26.27],
+    [98, 5, 5,  35, 122, 155, 24.26],
+    [93.20, 5, 4,  27, 137, 46, 25.95],
+    [93.99, 7, 5,  35, 150, 163, 23.25],
+
+    [103.16, 5, 5,  38, 188, 271, 33.31],
+    [98.75, 5, 5,  38, 803, 250, 31.01],
+    [88.80, 4, 4,  30, 643, 343, 30.55],
+    [92.07, 5, 5,  37, 662, 290, 33.46],
+
+    [100.16, 5, 5,  41, 84, 16, 33.19],
+    [96.95, 5, 4,  41, 84, 16, 31.43],
+    [88.17, 3, 4,  30, 86, 14, 26.20],
+    [93.98, 4, 4,  37, 82, 5, 24.23],
+]
+
+# Fill up the DataFrame with the provided numbers
+for col_index, col_values in enumerate(zip(*numbers_to_fill)):
+    for row_index, value in enumerate(col_values):
+        data_entry.at[row_index, columns[col_index]] = value
+
+# Scale the numbers and sum each row
+scaled_data = data_entry.copy()
+print("#####123")
+print(scaled_data)
+for col in columns:
+    scaled_values = scale_numbers(data_entry[col])
+    scaled_data[col] = scaled_values
+
+scaled_data['Total Score'] = scaled_data[column_score].sum(axis=1)
+
+# Find the best and worst total scores
+max_total_score = scaled_data['Total Score'].max()
+min_total_score = scaled_data['Total Score'].min()
+
+# HTML styling with inline styles for black text color, thicker black border lines, and increased width
+html_code = f"""
+
+    <div style="background-color:#f4f4f4;padding:20px;border-radius:10px">
+
+    <h1 style="color: #000; text-align: center;">Scores</h1>
+    <h5>Explore the captivating table that scores each season and pot, considering the crucial factors of weight grain , and a scaling system from 1 to 10. </h5>
+    <table style="width: 100%; border-collapse: collapse; margin-top: 20px;">
+        <tr>
+            <th style="border: 2px solid #000; padding: 10px;">Season</th>
+            <th style="border: 2px solid #000; padding: 10px;">Plot Number</th>
+            <th style="border: 2px solid #000; padding: 10px;">Score</th>
+        </tr>
+        {"".join(
+            f"<tr><td style='border: 2px solid #000; padding: 10px;'>{row['Season']}</td>"
+            f"<td style='border: 2px solid #000; padding: 10px;'>{row['Plot Number']}</td>"
+            f"<td style='border: 2px solid #000; padding: 10px; "
+            f"background-color: {get_color((10 - row['Total Score'])) };'>"
+            f"{row['Total Score']}</td></tr>"
+            for _, row in scaled_data.iterrows()
+        )}
+    </table>
+    </div>
+"""
+
+# Display the HTML content
+st.write("")
+st.markdown(html_code, unsafe_allow_html=True)
+
+
+
+
 option = st.sidebar.selectbox(
    "Select the Season...",
    ("Season1", "Season2", "Season3"),
